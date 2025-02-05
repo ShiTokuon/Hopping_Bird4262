56446c9e21b0c572090d3be8aab951461842c270
ShiTokuon <k021g1160@g.neec.ac.jp>
2025-02-05 23:43:50 +0900

    プレイヤーのモデルとアニメーションの追加

--- Assets/Scrips/PlayerController.cs
+++ Assets/Scrips/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UIElements;
+using SgLib;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
 
     public float jumpForce = 13f; // ジャンプパワー
 
+    public float rotateAngle = 360f; // プレイヤーの回転角
 
     [HideInInspector]
     public bool hasStarted = false;
@@ -21,9 +23,13 @@ public class PlayerController : MonoBehaviour
     [Header("Gameplay Preferences")]
     public GameManager gameManager;
     public GameObject player;
+    public AnimationClip jump;
+    public AnimationClip rotate;
 
     private Rigidbody rigid;
+    private Animator anim;
     private int turn = 1;
+    private bool isFinishRotate = false;
     private CameraController cameraController;
 
     void OnEnable()
@@ -51,14 +57,18 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        rigid = GetComponent<Rigidbody>();
         cameraController = Camera.main.GetComponent<CameraController>();
+
+        anim = player.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+
     }
     void Update()
     {
         if (Camera.main.WorldToScreenPoint(transform.position).y < -30 && GameManager.Instance.GameState != GameState.GameOver)
         {
             playerFallDown = true;
+            anim.Play(rotate.name);
             Die();
             Debug.Log("Player Fall Ground");
         }
@@ -74,14 +84,21 @@ public class PlayerController : MonoBehaviour
 
     void Flap()
     {
+        //SoundManager.Instance.PlaySound(SoundManager.Instance.flap);
         StartCoroutine(AddVelocityForPlayer()); //add velocity for player
 
+        if (!isFinishRotate)
+        {
+            isFinishRotate = true;
+            StartCoroutine(RotateParentPlayer()); //rotate player
+        }
     }
 
     IEnumerator AddVelocityForPlayer()
     {
         yield return new WaitForFixedUpdate();
         rigid.velocity = new Vector3(0, jumpForce, 0);
+        anim.SetTrigger(jump.name);
     }
 
     void Die()
@@ -98,16 +115,20 @@ public class PlayerController : MonoBehaviour
         // 障害物の当たり判定
 
         if (other.tag == "Enemy") //Hit gold
-        {
+            if (!hitObstacle)
+            {
+                hitObstacle = true;
 
-            //Destroy(other.gameObject);
-        }
-        else
-        {
-            // Hit obstacles
-            //if (!hitObstacle)
-            //cameraController.ShakeCamera();
-        }
+                Die();
+                rigid.velocity = new Vector3(0, 0, 0);
+                transform.position = new Vector3(0, transform.position.y, 0);
+
+                //Create particle base on obstacle
+                rigid.isKinematic = true;
+                StartCoroutine(WaitToDisableKinematic());
+
+                //SoundManager.Instance.PlaySound(SoundManager.Instance.hit);
+            }
 
         if (other.tag == "NormalObstacle") //hit obstacle
         {
@@ -130,12 +151,72 @@ public class PlayerController : MonoBehaviour
         if (GameManager.Instance.GameState == GameState.GameOver && col.collider.tag.Equals("Ground") && !hasHitGround)
         {
             hasHitGround = true;
+            //SoundManager.Instance.PlaySound(SoundManager.Instance.hit);
         }
+    }
+
+    IEnumerator RotateParentPlayer()
+    {
+        turn = turn * (-1);
+
+        float firstCurrentAngle = transform.eulerAngles.y; //Y rotation = 0
+        while (firstCurrentAngle < rotateAngle && GameManager.Instance.GameState == GameState.Playing) //Rotate 
+        {
+            float rotateAmount = 200 * Time.deltaTime;
+            firstCurrentAngle += rotateAmount;
+            if (turn < 0)
+            {
+                transform.Rotate(Vector3.up * rotateAmount);
+            }
+            else
+            {
+                transform.Rotate(Vector3.down * rotateAmount);
+            }
+            yield return null;
+        }
+        if (turn < 0)
+        {
+            transform.eulerAngles = new Vector3(0, rotateAngle, 0);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, -rotateAngle, 0);
+        }
+
+
+        float secondCurrentAngle = transform.eulerAngles.y; //Y rotation = rotateAngle
+        if (turn < 0)
+        {
+            while (secondCurrentAngle > 0 && GameManager.Instance.GameState == GameState.Playing)
+            {
+                // 回転スピード
+                float rotateAmount = 400 * Time.deltaTime;
+                secondCurrentAngle -= rotateAmount;
+                transform.Rotate(Vector3.down * rotateAmount);
+                yield return null;
+            }
+        }
+        else
+        {
+            while (secondCurrentAngle < 360 && GameManager.Instance.GameState == GameState.Playing)
+            {
+                // 回転スピード
+                float rotateAmount = 400 * Time.deltaTime;
+                secondCurrentAngle += rotateAmount;
+                transform.Rotate(Vector3.up * rotateAmount);
+                yield return null;
+            }
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, 0);
+
+        isFinishRotate = false;
     }
 
     IEnumerator WaitToDisableKinematic()
     {
         yield return new WaitForSeconds(0.5f);
+        anim.Play(rotate.name);
         rigid.isKinematic = false;
     }
 }
